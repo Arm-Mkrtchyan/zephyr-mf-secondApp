cbdc20c729d59130f46c6af165959b13fc4ca49d
armenmkrtchyan <arm.mkrtchyan07@gmail.com>
2026-02-26 00:25:14 +0400

    Fix tailwindcss integration.

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 import federation from '@originjs/vite-plugin-federation'
 import { withZephyr } from 'vite-plugin-zephyr';
 
 export default defineConfig({
   plugins: [
     react(),
+    tailwindcss(),
     withZephyr(),
     federation({
       name: 'second_app',
       remotes: {
-        first_app: 'https://arm-mkrtchyan07-gmail-com-17-first-app-zephyr-mf--bc72c0c9d-ze.zephyrcloud.app/assets/remoteEntry.js',
+        first_app: 'https://arm-mkrtchyan07-gmail-com-84-first-app-zephyr-mf--f9392d761-ze.zephyrcloud.app/assets/remoteEntry.js',
       },
       shared: ['react', 'react-dom'],
     }),
   ],
-  base: '/zephyr-mf-secondApp/',
+  base: '/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
