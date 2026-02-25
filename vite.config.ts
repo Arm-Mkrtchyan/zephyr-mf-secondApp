c477284641d0e3dd1a91011320547f1e4b6b8144
armenmkrtchyan <arm.mkrtchyan07@gmail.com>
2026-02-25 16:48:51 +0400

    Integrate Zephyr and fix config for Module Federation.

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,26 @@
-// second_app/vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import federation from '@originjs/vite-plugin-federation'
+import { withZephyr } from 'vite-plugin-zephyr';
 
 export default defineConfig({
   plugins: [
     react(),
+    withZephyr(),
     federation({
       name: 'second_app',
       remotes: {
-        first_app: 'http://localhost:5173/dist/assets/remoteEntry.js',
+        first_app: 'https://arm-mkrtchyan07-gmail-com-17-first-app-zephyr-mf--bc72c0c9d-ze.zephyrcloud.app/assets/remoteEntry.js',
       },
       shared: ['react', 'react-dom'],
     }),
   ],
+  base: '/zephyr-mf-secondApp/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    emptyOutDir: true,
+  },
   server: {
     port: 5174,
   },
